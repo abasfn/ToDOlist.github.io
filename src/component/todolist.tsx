a68ca46179a5ad3f9c20d859f6133428a4870b3d
tkhrtkrthrth+rth+5t+- <fakharanabas@gmail.com>
2021-06-28 12:41:30 +0430

    add inishial project

--- src/component/todolist.tsx
+++ src/component/todolist.tsx
@@ -3,42 +3,52 @@ import { useDispatch, useSelector } from 'react-redux';
 import { AddList, remove, setcheckboxes } from '../action';
 import './todolist.css';
 const ToDoList = () => {
+
     const container: any = useSelector(state => state);
     const dispach = useDispatch();
     const [input, setinpt] = useState<string>('')
     const onChangeInput = (e: any) => {
         setinpt(e.target.value)
     }
+
     const handleKeyPress = (e: any) => {
         if (e.key === 'Enter') {
-            dispach(AddList(input))
+            const initialState: any = [
+                { title: input, complete: false }
+            ]
+            dispach(AddList(initialState))
         }
     }
-    const deleteItem = (item: string) => {
-        dispach(remove(item))
-
+    const deleteItem = (index: any) => {
+        // debugger;
+        dispach(remove(index))
     }
-    const onChangeChekboxs = (item: string) => {
-        dispach(setcheckboxes(item))
+    const onChangeChekboxs = (index: any) => {
+        // debugger;
+        dispach(setcheckboxes(index))
     }
+    // debugger;
+    let arrey = container.filter((item: any) => item.complete === true);
+    console.log(arrey);
     return (
         <div className="contaner">
             <div>
                 <input onKeyPress={handleKeyPress} onChange={onChangeInput} />
-                {container.map((item: any) => {
+                {container.map((item: any, index: any) => {
                     return (
                         <div className="left">
                             <div className="item-and-chekboxs">
-                                {/* <input onClick={() => onChangeChekboxs(item)} className="checkbox" type="checkbox" /> */}
-                                <li>{item.title}</li>
+                                <input onClick={() => onChangeChekboxs(index)} className="checkbox" type="checkbox" />
+                                {item.complete === true ? <li><del>{item.title}</del></li>
+                                    : <li>{item.title}</li>}
                             </div>
-                            {/* <button onClick={() => deleteItem(item)} className="delete">Delete</button> */}
+                            <button onClick={() => deleteItem(item)} className="delete">Delete</button>
                         </div>
                     )
                 })}
                 <div className="text">
-                    {/* <h2>{container.length - complete.length}task left</h2>
-                <h2>{complete.length}task complete</h2> */}
+                    <h2>{container.length - arrey.length}task left</h2>
+                    <h2>{arrey.length}task complete</h2>
                 </div>
             </div>
             <div>
--- src/reduser/reduser.tsx
+++ src/reduser/reduser.tsx
@@ -2,30 +2,30 @@
 const initialState: any = [
     { title: "", complete: false }
 ]
-export const reduser = (state = initialState, action: any) => {
+export const reduser = (state: any = [], action: any) => {
     if (action.type === "ADD-LIST") {
         // const indexOf = state.data.indexOf(action.paylood)
         // if (indexOf >= 0) {
         //     return state
         // }
-        state = [...state]
-        state.title = action.paylood
+        state = [...state, ...action.paylood]
         return state;
     }
-    // else if (action.type === "REMOVE-LIST") {
-    //     state.data = state.data.filter((item: any) => !action.paylood.includes(item));
-    //     state.complete = state.complete.filter((item: any) => !action.paylood.includes(item));
-    //     return state;
-    // }
-    // else if (action.type === "SET-CHECK-BOXES") {
-    //     // debugger;
-    //     let indexof = state.complete.indexOf(action.paylood)
-    //     if (indexof >= 0) {
-    //         state.complete = state.complete.filter((item: any) => !action.paylood.includes(item));
-    //         return state
-    //     }
-    //     state.complete = [...state.complete, action.paylood];
-    // }
+    else if (action.type === "REMOVE-LIST") {
+        // debugger;
+        state = state.filter((item: any) => item != action.paylood)
+        return state;
+    }
+    else if (action.type === "SET-CHECK-BOXES") {
+        // debugger;
+        let indexof = state[action.paylood].complete
+        if (indexof === true) {
+            state[action.paylood].complete = false
+            return [...state]
+        }
+        state[action.paylood].complete = true
+        return [...state]
+    }
     return state
 }
 
